99c565a58f61194c0c832a7916ebd6a7427f2369
LBWyes <88589340+LBWyes@users.noreply.github.com>
2022-04-23 15:59:28 +0800

    修改了错误的m3版本

--- app/src/main/java/com/lbw/PrivacyKeeperV1/ui/MainActivity.kt
+++ app/src/main/java/com/lbw/PrivacyKeeperV1/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.lbw.PrivacyKeeperV1
+package com.lbw.PrivacyKeeperV1.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PrivacyKeeperV1Theme() {
+            PrivacyKeeperV1Theme {
 
             }
         }
