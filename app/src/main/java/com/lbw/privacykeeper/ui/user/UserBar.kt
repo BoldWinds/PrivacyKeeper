bba23af67c448d29dfc89ce9ebe98c7a47da693d
LBWyes <88589340+LBWyes@users.noreply.github.com>
2022-04-26 14:58:58 +0800

    进行了字符串资源的替换

--- app/src/main/java/com/lbw/privacykeeper/ui/user/UserBar.kt
+++ app/src/main/java/com/lbw/privacykeeper/ui/user/UserBar.kt
@@ -3,6 +3,7 @@ package com.lbw.privacykeeper.ui.user
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
@@ -13,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lbw.privacykeeper.ui.theme.PrivacyKeeperTheme
@@ -28,7 +30,6 @@ fun UserBar(
             .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ){
-
         Surface(
             modifier = Modifier
                 .clip(CircleShape)
@@ -51,7 +52,7 @@ fun UserBar(
             modifier = Modifier.padding(8.dp)
         ) {
             Text(
-                text = "Welcome Back!",
+                text = stringResource(id = R.string.welcome),
                 color = MaterialTheme.colorScheme.primary
             )
             Text(
@@ -67,7 +68,7 @@ fun UserBar(
         )
         {
             Icon(
-                painter = painterResource(id = R.drawable.ic_setting_foreground),
+                painter = painterResource(id = R.drawable.ic_edit_foreground),
                 contentDescription = "Setting",
                 modifier = Modifier
                     .clip(CircleShape)
@@ -87,4 +88,9 @@ fun PreviewUserBar() {
     PrivacyKeeperTheme {
         UserBar(userName = "LBW",{})
     }
+}
+
+@Composable
+fun ConfirmUpdateDialog() {
+    
 }
--- app/src/main/java/com/lbw/privacykeeper/ui/user/UserViewModel.kt
+++ app/src/main/java/com/lbw/privacykeeper/ui/user/UserViewModel.kt
@@ -1,2 +1,9 @@
 package com.lbw.privacykeeper.ui.user
 
+import com.lbw.privacykeeper.model.User
+
+class UserViewModel(
+    private val user : User,
+){
+    fun updatePassword() {}
+}
