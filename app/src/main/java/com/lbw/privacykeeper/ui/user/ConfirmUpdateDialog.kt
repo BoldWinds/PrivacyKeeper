e20c390e3a06480ad777ae524968b25c33dd4692
LBWyes <88589340+LBWyes@users.noreply.github.com>
2022-04-26 19:16:09 +0800

    完成了修改对话框的弹出

--- app/src/main/java/com/lbw/privacykeeper/ui/user/ConfirmUpdateDialog.kt
+++ app/src/main/java/com/lbw/privacykeeper/ui/user/ConfirmUpdateDialog.kt
@@ -36,8 +36,8 @@ fun ConfirmUpdateDialog(userViewModel: UserViewModel) {
             },
             text = {
                 Text(
-                    "This area typically contains the supportive text " +
-                            "which presents the details regarding the Dialog's purpose."
+                    text = stringResource(id = R.string.confirm_dialog_text),
+
                 )
             },
             confirmButton = {
